617ee3b27a4eced4798c4c5234303f079c59646d
Adrian Melchior <melchior@helliwood.com>
2020-05-14 16:54:50 +0200

    //WIP

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,28 @@
+Vue.component('todo-item', {
+    // The todo-item component now accepts a
+    // "prop", which is like a custom attribute.
+    // This prop is called todo.
+    props: ['todo'],
+    template: '<li id="">{{ todo.text }}</li>',
+})
+
+var data = {
+    dump: '',
+    groceryList: [
+        {id: 0, text: 'Vegetables'},
+        {id: 1, text: 'Cheese'},
+        {id: 2, text: 'Whatever else humans are supposed to eat'}
+    ]
+};
+
 var app = new Vue({
     el: '#app',
-    data: {
-        message: 'Hello Vue!',
-        seen: false,
-    }
-});
+    data: data
+})
+
+app.$watch('dump', function (newValue, oldValue) {
+    alert(newValue)
+})
+// data.dump = 'Dummy';
+c = console;
+c.log(app.groceryList[0] == data.groceryList[0]);
